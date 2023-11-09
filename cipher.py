6ea2a23618015f6b47b1f4258d1c6d626fe3506c
FranksNovak <fnovak78@gmail.com>
2023-11-09 18:00:04 +0100

    vymazal jsem alpha verzi kodu

--- cipher.py
+++ cipher.py
@@ -1,26 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# def encode(message, shift_number):
-#     shifted_text = ""
-#     for one_letter in message:
-#         if one_letter != " ":
-#             index = alphabet.index(one_letter)
-#             new_index = index + shift_number
-#             shifted_text += alphabet[new_index]
-#         else:
-#             shifted_text += one_letter
-#     print(f"Your encrypted text is {shifted_text}")
-
-# def decode(encrypted_message, shift_number):
-#     normal_text = ""
-#     for one_letter in encrypted_message:
-#         if one_letter != " ":
-#             index = alphabet.index(one_letter)
-#             new_index = index - shift_number
-#             normal_text += alphabet[new_index]
-#         else:
-#             normal_text += one_letter
-#     print(f"your decrypted text is {normal_text}")
 
 def cipher(start_text, shift_number, direction):
     end_text = ""
